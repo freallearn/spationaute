b1512bb53fd9dde239c64529c3bea7c64e76e9a5
Arnaud Fréalle <arnaud.frealle@gmail.com>
2026-06-01 15:21:33 +0200

    Init project spationaute files

---
+++ tests/unit-tests/main.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    //use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(1+ 2, 3);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        // This assert would fire and test will fail.
+        // Please note, that private functions can be tested too!
+        assert_eq!(1 + 2, 3);
+    }
+}
